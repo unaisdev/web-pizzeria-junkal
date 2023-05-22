8e613989a37e9ec6de2ce81b4b4f610e3a06f93d
unaicanales <unaicanales@gmail.com>
2023-05-22 22:15:39 +0200

    prueba

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -85,7 +85,7 @@ const Header = () => {
         <header className="fixed" >
             <div className={`${scrolled ? '-translate-y-20' : ''} flex justify-around px-1 py-2 transition duration-300 transform translate-y-0 border-b-2`}>
                 <a href="tel:943265863" className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2 fill-white" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2 fill-red-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
                         <path d="M15 7a2 2 0 0 1 2 2"></path>
