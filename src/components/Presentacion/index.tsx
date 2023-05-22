8380231840512e829a20641c740e53bee4f61e4d
unaicanales <unaicanales@gmail.com>
2023-05-22 16:57:51 +0200

    img vs swiper

---
+++ src/components/Presentacion/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+const Presentacion = () => {
+    return (
+        <div className='relative'>
+            <img className="presentation-img w-full object-cover object-top" src="/images/principal.webp" />
+
+            <div className="w-full h-60 presentation-shadow absolute bottom-0">
+
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Presentacion
