63a1b51ce3c93dbf5e8e256cd83282f3074f5221
unaicanales <unaicanales@gmail.com>
2023-05-23 22:28:27 +0200

    a

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -220,7 +220,7 @@ const Header = () => {
                 </div>
 
             </header >
-            <a href="tel:943265863" className={`${scrolled ? 'translate-x-0' : 'translate-x-24'} md:hidden flex items-center justify-center fixed bottom-12 right-8 w-12 h-12 rounded-full bg-green-500 z-50 shadow-md shadow-gray-600 transition transform duration-300 active:bg-green-700 active:shadow-sm active:shadow-gray-400 `}>
+            <a href="tel:943265863" className={`${scrolled ? 'translate-x-0' : 'translate-x-24'} border border-green-200 md:hidden flex items-center justify-center fixed bottom-12 right-8 w-12 h-12 rounded-full bg-green-500 z-50 shadow-md shadow-gray-600 transition transform duration-300 active:bg-green-700 active:shadow-sm active:shadow-gray-400 `}>
 
                 <svg xmlns="http://www.w3.org/2000/svg"  className="w-6 h-6 fill-green-900" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z" /></svg>
             </a>
