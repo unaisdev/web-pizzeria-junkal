7138772c0660d7004d55de0f9ef11b11b0012f9b
unaicanales <unaicanales@gmail.com>
2023-05-23 06:12:32 +0200

    updated seo description

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -97,16 +97,7 @@ const Header = () => {
             <header className={`${scrolled ? '-translate-y-6' : 'translate-y-0 '} fixed transition transform duration-300`} >
                 <div className={`flex justify-end items-center px-2 py-2 `}>
                     <div className="flex flex-col items-end">
-                        <a href="tel:943265863" className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2 fill-green-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
-                                <path d="M15 7a2 2 0 0 1 2 2"></path>
-                                <path d="M15 3a6 6 0 0 1 6 6"></path>
-                            </svg>
-
-                            <h6 className="text-xs sm:text-sm font-semibold flex text-white"><span className="font-light mr-1">+34</span> 943 26 58 63</h6>
-                        </a>
+                        
                         <a href="mailto:unaicanales@gmail.com" className={`${hideMail ? 'hidden': 'flex'} items-center`}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2 text-white" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -116,6 +107,16 @@ const Header = () => {
 
                         
                             <h6 className="text-xs sm:text-sm font-medium flex  text-white">pizzeriajunkal@gmail.com</h6>
+                        </a>
+                        <a href="tel:943265863" className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-2 fill-green-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
+                                <path d="M15 7a2 2 0 0 1 2 2"></path>
+                                <path d="M15 3a6 6 0 0 1 6 6"></path>
+                            </svg>
+
+                            <h6 className="text-xs sm:text-sm font-semibold flex text-white"><span className="font-light mr-1">+34</span> 943 26 58 63</h6>
                         </a>
                     </div>
 
