9fd74adf6f4b1565fb100c38bede2968b1920758
unaicanales <unaicanales@gmail.com>
2023-05-23 03:53:19 +0200

    seo

--- src/components/Presentacion/index.tsx
+++ src/components/Presentacion/index.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 const Presentacion = () => {
     return (
         <div className='relative'>
-            <img className="presentation-img w-full object-cover object-top" src="/images/principal.webp" />
+            <img className="presentation-img w-full object-cover object-top" src="/images/principal.webp" alt='Foto principal de presentacion pizzeria junkal irun'/>
 
             <div className="w-full h-60 presentation-shadow absolute bottom-0">
 
