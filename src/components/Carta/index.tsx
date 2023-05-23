dfe54cb686830865ce7ca6ff7e51f7d3ce55398f
unaicanales <unaicanales@gmail.com>
2023-05-23 03:08:58 +0200

    carta updated

--- src/components/Carta/index.tsx
+++ src/components/Carta/index.tsx
@@ -13,32 +13,37 @@ const Carta = () => {
     return (
         <section id="carta" className="pb-6">
 
-            <div className="flex flex-col">
-                <div className="py-6 pl-4 md:pl-12">
-                    <h1 className="font-bold text-xl md:text-3xl">NUESTRA CARTA</h1>
-                    <h2 className="text-gray-500 font-light text-sm md:text-md">GEURE MENUA</h2>
-                </div>
+            <div className="flex flex-col ">
+                <div className="bg-gray-100">
+                    <div className="py-6 pl-4  md:pl-12">
 
-                <div className="">
-                    <div className="mb-4 border-t border-gray-200 ">
-                        <ul className="flex flex-wrap justify-around -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
-                            <li className="mr-2" role="presentation">
-                                <button onClick={() => handleTabClick(1)} className={`${activeTab === 1 ? "bg-gray-300 text-black" : "bg-gray-100 text-gray-500"} text-md md:text-xl uppercase inline-block px-4 py-2 border-b-2 border-transparent rounded-b-lg hover:bg-gray-200 `} id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Pizzas</button>
+                        <h1 className="font-bold text-xl md:text-3xl">NUESTRA CARTA</h1>
+                        <h2 className="text-gray-500 font-light text-sm md:text-md">GEURE MENUA</h2>
+                    </div>
+                    <div className="">
+                        <ul className="flex flex-wrap justify-around text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+                            <li className="" role="presentation">
+                                <button onClick={() => handleTabClick(1)} className={`${activeTab === 1 ? "bg-gray-300 hover:bg-gray-300 text-black" : "bg-gray-100 text-gray-400 hover:bg-gray-200"} text-md md:text-xl uppercase px-4 py-2 rounded-t-lg  `} id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Pizzas</button>
                             </li>
-                            <li className="mr-2" role="presentation">
-                                <button onClick={() => handleTabClick(2)}  className={`${activeTab === 2 ? "bg-gray-300 text-black" : "bg-gray-100 text-gray-500"} text-md md:text-xl uppercase inline-block px-4 py-2 border-b-2 border-transparent rounded-b-lg hover:bg-gray-200`} id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Postres</button>
+                            <li className="" role="presentation">
+                                <button onClick={() => handleTabClick(2)} className={`${activeTab === 2 ? "bg-gray-300 hover:bg-gray-300 text-black" : "bg-gray-100 text-gray-400 hover:bg-gray-200"} text-md md:text-xl uppercase inline-block px-4 py-2 rounded-t-lg`} id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Postres</button>
                             </li>
-                            <li className="mr-2" role="presentation">
-                                <button onClick={() => handleTabClick(3)}  className={`${activeTab === 3 ? "bg-gray-300 text-black" : "bg-gray-100 text-gray-500"} text-md md:text-xl uppercase inline-block px-4 py-2 border-b-2 border-transparent rounded-b-lg hover:bg-gray-200`} id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Bebidas</button>
+                            <li className="" role="presentation">
+                                <button onClick={() => handleTabClick(3)} className={`${activeTab === 3 ? "bg-gray-300 hover:bg-gray-300 text-black" : "bg-gray-100 text-gray-400 hover:bg-gray-200"} text-md md:text-xl uppercase inline-block px-4 py-2 rounded-t-lg`} id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Bebidas</button>
                             </li>
 
                         </ul>
                     </div>
-                    
+                </div>
+
+
+                <div className="">
+
+
                     <div className="">
                         {activeTab === 1 &&
                             <div>
-                                <div className="flex flex-wrap justify-center overflow-auto">
+                                <div className="flex flex-wrap justify-center overflow-auto py-4">
 
                                     <div className="item-carta flex h-auto my-2 mx-4 items-center justify-around bg-slate-200">
                                         <img className="h-56 w-36 sm:w-56 object-center object-cover p-2 aspect-square" src="https://images.unsplash.com/photo-1574071318508-1cdbab80d002?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80" />
