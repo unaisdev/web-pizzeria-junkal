1573f3ecdb42ac8580d2d029fea0464e42cc85aa
Unai Canales <36845145+unaisdev@users.noreply.github.com>
2024-01-28 05:48:26 +0100

    feat: Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image()]
+  integrations: [react(), tailwind(), image()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
